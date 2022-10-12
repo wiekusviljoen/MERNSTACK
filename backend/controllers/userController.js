ad2b4a61e2a86b7b26509007025c7b8b7edd7f7c
wiekusviljoen <wiekusviljoen@gmail.com>
2022-10-12 15:30:53 +0200

    commit on master

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,26 +3,35 @@ const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
+// @desc    Register new user
+// @route   POST /api/users
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     res.status(400);
-    throw new Error("Please enter all details");
+    throw new Error("Please add all fields");
   }
 
-  //check if exist
-  const userExist = await User.findOne({ email });
+  // Check if user exists
+  const userExists = await User.findOne({ email });
 
-  if (userExist) {
+  if (userExists) {
     res.status(400);
-    throw new Error("user exists");
+    throw new Error("User already exists");
   }
 
+  // Hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  const user = await User.create({ name, email, password: hashedPassword });
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
 
   if (user) {
     res.status(201).json({
@@ -33,12 +42,17 @@ const registerUser = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(400);
-    throw new Error("INVALID user data");
+    throw new Error("Invalid user data");
   }
 });
 
+// @desc    Authenticate a user
+// @route   POST /api/users/login
+// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+
+  // Check for user email
   const user = await User.findOne({ email });
 
   if (user && (await bcrypt.compare(password, user.password))) {
@@ -52,14 +66,16 @@ const loginUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Invalid credentials");
   }
-
-  res.json({ message: " Login User" });
 });
 
+// @desc    Get user data
+// @route   GET /api/users/me
+// @access  Private
 const getMe = asyncHandler(async (req, res) => {
   res.status(200).json(req.user);
 });
 
+// Generate JWT
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
